85c0b118f3d59338583ccfbcb7b402df429598d5
jcraney143 <147489241+jcraney143@users.noreply.github.com>
2026-02-13 17:53:24 +0000

    Update utils.py

--- ingest-service/utils.py
+++ ingest-service/utils.py
@@ -1,1 +1,22 @@
+import uuid
+import json
+import logging
+from google.cloud import pubsub_v1
+
+logger = logging.getLogger("ingest-service")
+logger.setLevel(logging.INFO)
+
+publisher = pubsub_v1.PublisherClient()
+
+def generate_id():
+    return str(uuid.uuid4())
+
+def publish(topic: str, project: str, message: dict):
+    topic_path = publisher.topic_path(project, topic)
+    data = json.dumps(message).encode("utf-8")
+    future = publisher.publish(topic_path, data)
+    return future.result()
+
+def log(message: str, **kwargs):
+    logger.info(json.dumps({"message": message, **kwargs}))
 
