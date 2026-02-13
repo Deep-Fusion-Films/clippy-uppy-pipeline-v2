6620145f6065e00a25ad7b06c4abd97c9710d3f1
jcraney143 <147489241+jcraney143@users.noreply.github.com>
2026-02-13 18:04:55 +0000

    Update utils.py

--- store-service/utils.py
+++ store-service/utils.py
@@ -1,1 +1,14 @@
+import json
+import logging
+from google.cloud import firestore, storage
+
+logger = logging.getLogger("store-service")
+logger.setLevel(logging.INFO)
+
+firestore_client = firestore.Client()
+storage_client = storage.Client()
+
+
+def log(message: str, **kwargs):
+    logger.info(json.dumps({"message": message, **kwargs}))
 
