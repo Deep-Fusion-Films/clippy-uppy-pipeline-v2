47a4320214417bc432194812f0c6a8da41d9b8a3
jcraney143 <147489241+jcraney143@users.noreply.github.com>
2026-02-13 17:59:05 +0000

    Update utils.py

--- frame-sample-service/utils.py
+++ frame-sample-service/utils.py
@@ -1,1 +1,40 @@
+import uuid
+import json
+import logging
+from google.cloud import storage, pubsub_v1
+
+logger = logging.getLogger("frame-sample-service")
+logger.setLevel(logging.INFO)
+
+storage_client = storage.Client()
+publisher = pubsub_v1.PublisherClient()
+
+
+def log(message: str, **kwargs):
+    logger.info(json.dumps({"message": message, **kwargs}))
+
+
+def generate_id():
+    return str(uuid.uuid4())
+
+
+def download_from_gcs(bucket: str, file_name: str, local_path: str):
+    bucket_obj = storage_client.bucket(bucket)
+    blob = bucket_obj.blob(file_name)
+    blob.download_to_filename(local_path)
+    log("Downloaded video", bucket=bucket, file=file_name)
+
+
+def upload_to_gcs(bucket: str, file_name: str, local_path: str):
+    bucket_obj = storage_client.bucket(bucket)
+    blob = bucket_obj.blob(file_name)
+    blob.upload_from_filename(local_path)
+    log("Uploaded frame", bucket=bucket, file=file_name)
+
+
+def publish(topic: str, project: str, message: dict):
+    topic_path = publisher.topic_path(project, topic)
+    data = json.dumps(message).encode("utf-8")
+    future = publisher.publish(topic_path, data)
+    return future.result()
 
